9b9c82aff844eb0d2f15aa7607d06169523471f6
Seth Rhodes <iamsethrhodes@gmail.com>
2023-10-30 17:38:44 -0400

    changed more paths

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -8,20 +8,19 @@ import {
 } from "react";
 
 export default function Carousel({}) {
-  const baseUrl = "https://sethrhod.github.io/portfolio-project";
 
   const images: string[] = [
-    `${baseUrl}/2.png`,
-    `${baseUrl}/1.png`,
-    `${baseUrl}/3.png`,
-    `${baseUrl}/6.png`,
-    `${baseUrl}/4.png`,
-    `${baseUrl}/5.png`,
-    `${baseUrl}/2.png`,
-    `${baseUrl}/1.png`,
-    `${baseUrl}/3.png`,
-    `${baseUrl}/6.png`,
-    `${baseUrl}/4.png`,
+    `/2.png`,
+    `/1.png`,
+    `/3.png`,
+    `/6.png`,
+    `/4.png`,
+    `/5.png`,
+    `/2.png`,
+    `/1.png`,
+    `/3.png`,
+    `/6.png`,
+    `/4.png`,
   ];
 
   //if screen is mobile set perView to 1
